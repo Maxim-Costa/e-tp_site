3e250193c58585bd80ac9c1475368dfaa9b9db36
Maxim-Costa <mxmcosta@gmail.com>
2020-08-30 23:25:08 +0200

    trim

--- src/PostTable.php
+++ src/PostTable.php
@@ -45,7 +45,7 @@ class PostTable
     public static function UpdateUser(PDO $pdo, int $id, string $email, string $pseudo, int $discord, string $password)
     {
         $query = $pdo->prepare("UPDATE user_etphoste set email_user = :email, speudo_user = :pseudo, discord_user = :discord, password_user = :password where id_user = :id;");
-        $ok = $query->execute(array('id' => $id, 'email' => strtolower($email), 'pseudo' => $pseudo, 'discord' => $discord, 'password' => $password));
+        $ok = $query->execute(array('id' => $id, 'email' => trim(strtolower($email)), 'pseudo' => $pseudo, 'discord' => $discord, 'password' => $password));
         if ($ok === false) {
             throw new \Exception("Impossible d'éditer la valeur 1");
         }
@@ -128,7 +128,7 @@ class PostTable
     public static function findByEmail(PDO $pdo, string $email)
     {
         $query = $pdo->prepare("SELECT id_user,email_user,speudo_user,password_user,role_user FROM etphoste_client.user_etphoste where email_user = :email");
-        $query->execute(array('email' => strtolower($email)));
+        $query->execute(array('email' => trim(strtolower($email))));
         $query->setFetchMode(PDO::FETCH_OBJ);
         $posts = $query->fetch();
 
@@ -152,7 +152,7 @@ class PostTable
     public static function NewUser(PDO $pdo, string $email, string $password, string $pseudo)
     {
         $query = $pdo->prepare("INSERT INTO `user_etphoste`( `email_user`, `password_user`, `speudo_user` ) VALUES( :email, :password, :pseudo )");
-        $ok = $query->execute(array('email' => strtolower($email), 'password' => $password, 'pseudo' => $pseudo));
+        $ok = $query->execute(array('email' => trim(strtolower($email)), 'password' => $password, 'pseudo' => $pseudo));
         return $ok;
     }
 
