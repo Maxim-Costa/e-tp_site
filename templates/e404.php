2ee9b6c53bc9ebd3c60dfa129269be271f3ae019
Maxim-Costa <mxmcosta@gmail.com>
2020-09-03 00:00:30 +0200

    fix chr

--- templates/e404.php
+++ templates/e404.php
@@ -8,7 +8,7 @@ http_response_code(404);
     <div class="card border-warning mt-5" style="max-width: 25rem">
         <img src="https://i.pinimg.com/originals/7d/21/02/7d21021dac6bc10c72ab13801588415b.jpg" alt="" class="card-img-top">
         <div class="card-body text-warning">
-            <h5 class="card-title">Tu t'es tromper de chemain</h5>
+            <h5 class="card-title">Tu t'es trompé de chemin</h5>
             <a href="<?=$router->generate('home')?>" class="btn btn-outline-warning">Go Home</a>
         </div>
     </div>
