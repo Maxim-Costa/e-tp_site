49886f72d4e514505f2f4c909355ce425d8a5d99
Maxim-Costa <mxmcosta@gmail.com>
2020-09-07 22:30:21 +0200

    change addr Connection.php

--- src/Connection.php
+++ src/Connection.php
@@ -7,7 +7,7 @@ class Connection {
 
     public static function getPDO (): PDO
     {
-        return new PDO('mysql:dbname=etphoste_client;host=45.140.165.13;charset=utf8', 'etphoste_client', 'Maxim1447', [
+        return new PDO('mysql:dbname=etphoste_client;host=127.0.0.1;charset=utf8', 'etphoste_client', 'Maxim1447', [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
         ]);
     }
